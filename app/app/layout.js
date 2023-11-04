cb48d2ade5fedbfcc36d7e628b058cb23339690e
Clarksonlawson <74101434+Clarksonlawson@users.noreply.github.com>
2023-11-04 11:53:28 +0100

    ggg

--- app/app/layout.js
+++ app/app/layout.js
@@ -1,11 +1,11 @@
 import { Inter } from 'next/font/google'
 // import './globals.css'
-import Head from '@/app/ui/Head/head'
+import Head from '@/app/ui/head/head'
 import Loader from './ui/Loader/loader'
 import Cursor from './ui/Cursor/cursor'
 import progressScroll from './ui/progressScroll/progress'
 import Navbar from './ui/navBar/navBar'
-import Footer from './ui/Footer/Footer'
+import Footer from './ui/footer/Footer'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
